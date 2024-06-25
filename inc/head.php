fb768f57343ac884770b7019d30c31ec555f7953
shokomaeda <shokosuzuki1118@gmail.com>
2024-06-25 20:29:22 +0900

    リセットcss変更

--- inc/head.php
+++ inc/head.php
@@ -1,5 +1,5 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?php echo $title; ?></title>
-<link rel="stylesheet" href="https://unpkg.com/destyle.css@3.0.2/destyle.min.css">
+<link rel="stylesheet" href="<?php echo $path; ?>css/reset.css">
 <link rel="stylesheet" href="<?php echo $path; ?>css/common.css">
